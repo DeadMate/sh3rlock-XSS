c5f58013e42d79f0a147694024429d260969c31a
root <root@localhost.localdomain>
2017-09-22 14:34:42 -0400

    Installation Fix

--- firefox-update.sh
+++
@@ -1,5 +0,0 @@
-wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-mkdir /opt/firefox
-tar xjf FirefoxSetup.tar.bz2 -C /opt/firefox/
-mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr_orig
-ln -s /opt/firefox/firefox/firefox /usr/lib/firefox-esr/firefox-esr
